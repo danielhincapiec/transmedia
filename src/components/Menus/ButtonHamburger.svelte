e152ca6bc7dbe27070d31c21a34f4b635250f799
dh <you@example.comgit config --global user.name dhgit config --global user.email daniel.hincapie.cubides@gmail.com>
2025-06-19 01:20:28 -0500

    ajustes de container

--- src/components/Menus/ButtonHamburger.svelte
+++ src/components/Menus/ButtonHamburger.svelte
@@ -7,7 +7,7 @@
 
 <button
 	on:click={toggleClass}
-	class="flex justify-between flex-col cursor-pointer p-2 size-10 flex-shrink-0"
+	class="flex justify-between flex-col cursor-pointer p-2 size-8 flex-shrink-0"
 >
 	<div class="line h-[2px] bg-black"></div>
 	<div class="line h-[2px] bg-black"></div>
